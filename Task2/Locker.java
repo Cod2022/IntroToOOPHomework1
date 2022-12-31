f6155c68b623521e2befa7b6834072e34060fb1c
My Name <Me@gmail.com>
2022-12-31 16:30:06 +0400

    2nd task started

---
+++ Task2/Locker.java
@@ -0,0 +1,9 @@
+package Task2;
+
+public class Locker {
+    boolean opened;
+
+    public Locker() {
+        this.opened = false;
+    }
+}
---
+++ Task2/Man.java
@@ -0,0 +1,9 @@
+package Task2;
+
+public class Man {
+    String name;
+
+    public Man (String name) {
+        this.name = name;
+    }
+}
